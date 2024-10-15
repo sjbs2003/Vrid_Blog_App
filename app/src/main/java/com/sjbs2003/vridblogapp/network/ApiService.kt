fd5c059b86e4127eca0a907a6258808ab237b43a
vicki <surajbarik2003@gmail.com>
2024-10-15 23:12:03 +0530

    built home screen and viewModel

--- app/src/main/java/com/sjbs2003/vridblogapp/network/ApiService.kt
+++ app/src/main/java/com/sjbs2003/vridblogapp/network/ApiService.kt
@@ -1,9 +1,9 @@
 package com.sjbs2003.vridblogapp.network
 
-import com.sjbs2003.vridblogapp.model.BlogPost
+import com.sjbs2003.vridblogapp.model.BlogPostData
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("posts")
-    fun getBlogList() : List<BlogPost>
+    fun getBlogList() : List<BlogPostData>
 }
