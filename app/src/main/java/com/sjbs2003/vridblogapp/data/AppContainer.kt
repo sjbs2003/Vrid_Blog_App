e93722047bf266f08b9ba3265fa1f3a7fbcd668d
vicki <surajbarik2003@gmail.com>
2024-10-15 22:08:48 +0530

    data layer is done

--- app/src/main/java/com/sjbs2003/vridblogapp/data/AppContainer.kt
+++ app/src/main/java/com/sjbs2003/vridblogapp/data/AppContainer.kt
@@ -1,6 +1,9 @@
 package com.sjbs2003.vridblogapp.data
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.sjbs2003.vridblogapp.network.ApiService
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
@@ -11,13 +14,23 @@ interface AppContainer {
 class DefaultContainer : AppContainer {
 
     private val json = Json { ignoreUnknownKeys = true }
+
+    private val baseurl =
+        "https://blog.vrid.in/wp-json/wp/v2/"
+
     private val httpClient = OkHttpClient.Builder().build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseurl)
         .client(httpClient)
-        .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    private val retrofitService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
 
+    override val blogRepository: BlogRepository by lazy {
+        NetworkBlogRepo(retrofitService)
+    }
 }
